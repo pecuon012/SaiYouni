98029f47212ab9fc0d6686c7e40bfe78361ed505
unknown <xipo.tan@gmail.com>
2025-03-29 01:05:49 +0700

    init

--- Assets/Week 1/Scripts/Enemy.cs
+++ Assets/Week 1/Scripts/Enemy.cs
@@ -4,29 +4,33 @@ using UnityEngine;
 
 public class Enemy : MonoBehaviour
 {
-
     // 5 Enemies variables
-
-    //variables 1 
-
-    //variables 2
-
-    //variables 3 
-
-    //variables 4 
-
-    //variables 5 
-
+    int CurrentHp = 20; //variables 1
+    float head = 100f;  //variables 2
+    bool Isdead = true; //variables 3 
+    string monster_name = "jn"; //variables 4 
+    double boss = 1000000000000; //variables 5 
 
     // 5 Enemies methods
+    public virtual int CurrentHp1() //Method 1
+    {
+        return CurrentHp;
+    }
+    public virtual float GetMheadaxWeight2() //Method 2
+    {
+        return head;
+    }
+    public virtual bool Isdead3() //Method 3
+    {
+        return this.Isdead;
+    }
+    public virtual string monster_name4() //Method 4
+    {
+        return monster_name;
+    }
+    public virtual double boss5() //Method 5
+    {
+        return boss;
+    }
 
-    //Method 1
-
-    //Method 2
-
-    //Method 3
-
-    //Method 4
-
-    //Method 5
 }
